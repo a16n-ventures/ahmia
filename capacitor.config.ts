5dcccc1f21981a203d56e6a178a1eea7021cb36b
barack300 <amdagas@yahoo.com>
2026-01-09 11:50:07 +0100

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.89e81f7e01ce4d72a4a7e6826e8b7b3a',
-  appName: 'lynq',
+  appId: 'com.ahmia.app',
+  appName: 'ahmia',
   webDir: 'dist',
   server: {
-    url: "https://89e81f7e-01ce-4d72-a4a7-e6826e8b7b3a.lovableproject.com?forceHideBadge=true",
-    cleartext: true
+    androidScheme: 'https',
   },
 };
 
